96d6a9aff465fd53c3928bc10a495582f2a97588
Indy Caluwe <indy.caluwe@immalle.eu>
2017-12-17 00:08:43 +0100

    Days Nieuwjaar

--- Oef exaam datetime/Program.cs
+++ Oef exaam datetime/Program.cs
@@ -10,6 +10,29 @@ namespace Oef_exaam_datetime
     {
         static void Main(string[] args)
         {
+            DateTime jaar = new DateTime(2017, 1, 1);
+
+             DateTime nieuwjaar = new DateTime(2027, 1, 1);
+
+
+             while (jaar.Year < nieuwjaar.Year)
+             {
+                 jaar = jaar.AddYears(1);
+                 Console.WriteLine("{0} : Het is vandaag {1}, Happy Nieuwjaar!!! ", jaar.Year, jaar.DayOfWeek);
+            }
+
+            //DateTime verjaardag = new DateTime(2001, 11, 19);
+
+           // for (int x = 0; x <= 100; x++)
+            //{
+            //    verjaardag = verjaardag.AddYears(1);
+            //
+             //   if (verjaardag.DayOfWeek == DayOfWeek.Wednesday)
+             //   {
+             //       Console.WriteLine("in {0} verjaar je op woensdag ", verjaardag.Year);
+            //
+             //   }
+             // }
         }
     }
 }
